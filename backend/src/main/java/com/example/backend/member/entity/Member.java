33e3d04b21f48d4d1cf360c39ce3d7dd94dfa636
AReuma <kuuniin@gmail.com>
2024-01-11 00:35:38 +0900

    fix: Member 엔티티 Builder 변경, SocialType 일반 로그인 회원 추가 [MMS]

--- backend/src/main/java/com/example/backend/member/entity/Member.java
+++ backend/src/main/java/com/example/backend/member/entity/Member.java
@@ -2,11 +2,14 @@ package com.example.backend.member.entity;
 
 import com.example.backend.common.TimeStamped;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
 @Getter
-@Builder(toBuilder = true)
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends TimeStamped {
@@ -15,12 +18,16 @@ public class Member extends TimeStamped {
     @Column(name = "member_id")
     private Long id;
 
+    @Email
     private String username;
 
+    @NotBlank
     private String password;
 
+    @NotBlank
     private String nickname;
 
+    @NotBlank
     private String phoneNum;
 
     @Enumerated(EnumType.STRING)
--- backend/src/main/java/com/example/backend/member/entity/SocialType.java
+++ backend/src/main/java/com/example/backend/member/entity/SocialType.java
@@ -1,5 +1,5 @@
 package com.example.backend.member.entity;
 
 public enum SocialType {
-    KAKAO, GITHUB, GOOGLE
+    KAKAO, GITHUB, GOOGLE, DEFAULT
 }
