aa76f256fd5b322568cff4d0b53a9b2b6e95c5e7
AReuma <kuuniin@gmail.com>
2024-01-26 23:08:08 +0900

    feat: 메인화면 구현, 회원일 경우 회원이 가입한 카테고리로 정렬, 로그인 전일 경우 아티스트 이름순으로 정렬 구현 [IMS]

--- frontend/src/constant/ApiUrl/ApiUrl.js
+++ frontend/src/constant/ApiUrl/ApiUrl.js
@@ -4,3 +4,5 @@ export const AWS_API_URL  = 'http://13.134.168.202'
 export const KAKAO_AUTH_URL = 'http://localhost:7777/api/v1/auth/oauth2/kakao'
 export const NAVER_AUTH_URL = 'http://localhost:7777/api/v1/auth/oauth2/naver'
 export const GOOGLE_AUTH_URL = 'http://localhost:7777/api/v1/auth/oauth2/google'
+
+export const ARTIST_CATEGORY = "/api/v1/idol"
