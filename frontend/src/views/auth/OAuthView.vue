ee39aa3f06cebb486302c8dedff2ce1d6f3e2b1b
AReuma <kuuniin@gmail.com>
2024-01-27 21:52:58 +0900

    fix: sns 로그인 쿠키 만료시간 오류 해결 [MMS]

--- frontend/src/views/auth/OAuthView.vue
+++ frontend/src/views/auth/OAuthView.vue
@@ -5,7 +5,6 @@
 <script>
 import {defineComponent} from 'vue'
 import {useCookie} from "vue-cookie-next";
-import {ACCESS_TOKEN_EXPIRE, REFRESH_TOKEN_EXPIRE} from "@/constant/jwt/JwtUtil";
 import {ParsingInfo} from "@/constant/jwt/JwtParser";
 
 export default defineComponent({
@@ -17,25 +16,16 @@ export default defineComponent({
     };
   },
   mounted() {
-    let token = this.$route.query;
-    this.accessToken = token.accessToken;
-    this.refreshToken = token.refreshToken;
-
-    useCookie().setCookie('accessToken', this.accessToken, {
-      expire: ACCESS_TOKEN_EXPIRE,
-    });
-    useCookie().setCookie('refreshToken', this.refreshToken, {
-      expire: REFRESH_TOKEN_EXPIRE,
-    });
+    this.accessToken = useCookie().getCookie('accessToken');
 
     const openerWindow = window.opener;
     const routeHome = this.$router.resolve(
         {
           path: '/',
-          name: 'HomeView',
+          name: 'MainView',
         });
 
-    ParsingInfo(token.accessToken)
+    ParsingInfo(this.accessToken)
 
     openerWindow.document.location.href = routeHome.href;
     window.close();
